0bf6841b175e74eb05458789e9ebb3462f202660
petialetia <petialetia@yandex.ru>
2024-05-16 01:57:12 +0300

    sleep section is added for poller.sh

--- poller/poller.sh
+++ poller/poller.sh
@@ -1,4 +1,6 @@
 SERVER_SERVICE_SERVICE_HOST=127.0.0.1
 SERVER_SERVICE_SERVICE_PORT=8000
 
+sleep 60s
+
 while true; do echo $(curl -s http://${SERVER_SERVICE_SERVICE_HOST}:${SERVER_SERVICE_SERVICE_PORT}/statistics) >> /logs/log.txt; sleep 5s; done
