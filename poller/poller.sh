806b256ee4b9eade9ba4d1f883829eed9147421d
petialetia <petialetia@yandex.ru>
2024-05-18 23:43:42 +0300

    better poller

--- poller/poller.sh
+++ poller/poller.sh
@@ -1,7 +1,7 @@
 SERVER_SERVICE_SERVICE_HOST=127.0.0.1
 SERVER_SERVICE_SERVICE_PORT=8000
 
-echo ${SERVER_SERVICE_SERVICE_HOST} >> logs/log.txt
-echo ${SERVER_SERVICE_SERVICE_PORT} >> logs/log.txt
+echo ${SERVER_SERVICE_SERVICE_HOST} >> /logs/log.txt
+echo ${SERVER_SERVICE_SERVICE_PORT} >> /logs/log.txt
 
 # while true; do curl -s http://${SERVER_SERVICE_SERVICE_HOST}:${SERVER_SERVICE_SERVICE_PORT}/statistics >> /logs/log.txt; sleep 5s; done
